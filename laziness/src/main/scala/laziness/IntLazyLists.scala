fedb254406e86b76b93a9002256057cdf2828540
franziska-a <147037823+franziska-a@users.noreply.github.com>
2024-06-14 13:42:02 +0200

    removed ackermann skeleton

--- laziness/src/main/scala/laziness/IntLazyLists.scala
+++ laziness/src/main/scala/laziness/IntLazyLists.scala
@@ -28,11 +28,3 @@ object IntLazyLists:
 
   lazy val fib: MyLazyList[Int] =
     cons(0, cons(1, ???))
-
-  def ack(m: Int, n: Int): Int =
-    if m == 0 then n + 1
-    else if n == 0 then ack(m - 1, 1)
-    else ack(m - 1, ack(m, n - 1))
-
-  lazy val ackermann: MyLazyList[MyLazyList[Int]] =
-    ???
